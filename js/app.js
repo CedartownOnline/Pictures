244ce7961152206704e8f676bab3357e8a7d6834
James F Thompson <james@cedartownonline.com>
2015-08-04 19:03:05 -0400

    Release Version 1
    First Release

--- js/app.js
+++ js/app.js
@@ -1,86 +1,96 @@
-var polkhistApp = angular.module('polkhistApp', ['ngRoute']);
-	
-	// configure our routes
-	polkhistApp.config(function($routeProvider) {
-		$routeProvider
-		// route for the home page
-			.when('/', {
-				templateUrl: '/pages/home.html'
-			})
-			// route for the about page
-			.when('/about', {
-				templateUrl: '/pages/about.html'
-			})
-			// route for the Links page
-			.when('/calendar', {
-				templateUrl: '/pages/calendar.html'
-			})
-			// route for the Links page
-			.when('/links', {
-				templateUrl: '/pages/links.html',
-				controller: 'LinksController'
-			})
-			// route for the museum page
-			.when('/museum', {
-				templateUrl: '/pages/museum.html'
-			})
-			// route for the museum page
-			.when('/shop', {
-				templateUrl: '/pages/shop.html'
-			})
-			// route for the info page
-			.when('/info', {
-				templateUrl: '/pages/info.html'
-			})
-			// route for the Membership page
-			.when('/membership', {
-				templateUrl: '/pages/membership.html',
-				controller: 'MembershipController'
-			})
-			// route for the contact page
-			.when('/contact', {
-				templateUrl: '/pages/contact.html'
-			});
-	});
-	
-	// create the controller and inject Angular's $scope
-	polkhistApp.controller('MainController', function($scope,  $http) {
-		$http.get('/data/calendar.json').then(function(res) {
-			var events = res.data;
-			var len = events.length;
-			var cdate;
-			$scope.events = [];
-			for (i = 0; i < len; i++) {
-			    var event = events[i]; 
-				cdate = moment(event.date);
-				ddate=0 - moment().diff(cdate,'days');
-				if (ddate >= 0) {
-					event.interDate = cdate;					
-					if (ddate == 0) {
-						if (!events[i].tags) {event.tags=[];}
-					    event.tags.push("Today");
-						$scope.happeningToday = event;						
-						$scope.showAlert=true;
-					}
-					$scope.events.push(event);
-				}
-			}
-			$scope.events.sort(function(a, b) {
-				return a.interDate > b.interDate
-			});
+var picturesApp = angular.module('picturesApp', ['ngRoute','ui.bootstrap']);
+
+// configure our routes
+picturesApp.config(function($routeProvider) {
+	$routeProvider
+	// route for the home page
+		.when('/', {
+			templateUrl: '/pages/home.html'
+		})
+		// route for the pictures page
+		.when('/pictures/:path?', {
+			templateUrl: '/pages/pictures.html',
+			controller: 'picturesController'
+		})
+		.otherwise({
+			redirectTo: "/"
 		});
-	});
+});
+
+// create the controller and inject Angular's $scope
+picturesApp.controller('MainController', function($scope, $http) {});
+
+picturesApp.controller('picturesController', function($scope, $http, $routeParams) {
 	
+	$scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+	$scope.directories = [];
+	$scope.slides = [];
 	
-	polkhistApp.controller('LinksController', function($scope, $http) {
-		$http.get('/data/links.json').then(function(res) {
-			$scope.links = res.data;
+	if ($routeParams.path) {
+		$scope.path = $routeParams.path.split("$");
+		if ($scope.path[0] == ""){
+			$scope.path.shift();
+		}
+		$scope.dirPath = "/" + $scope.path.join("/");
+	}
+	else {
+		$scope.path = [];
+		$scope.dirPath = "";
+	}
+	$scope.pathString = "/pictures" + $scope.dirPath;
+	
+	var dirPath = $scope.path.slice();
+    dirPath.push("directory.json");
+	dirPathString = "/" + dirPath.join("/");
+	
+	$http.get(dirPathString)
+		.then(function(res) {
+		   var data = res.data;
+	       for (i = 0, len = data.directories.length; i < len; i++){
+			   
+			   var wpath =  data.directories[i].path;
+			   wpath = wpath.split("/")
+			   if (wpath[0] == "") {wpath.shift()}
+			   wpath=wpath.join("$");
+			   data.directories[i].path = "/pictures/" + wpath;
+			   
+			   if (data.directories[i].files) {
+				data.directories[i].fileCount = data.directories[i].files.length;
+			   } else {
+				 data.directories[i].fileCount = 0;  
+			   }
+		   }
+		   $scope.directories = data.directories;
+		   if ($scope.dirPath != "/"){
+			   var dir = {}
+			   dir.name = "Parent"
+			   var parentPath = $scope.path.slice();
+			   parentPath.pop();
+			   
+			   dir.path = "/pictures/" + parentPath.join("$");
+			   $scope.directories.unshift(dir);
+		   }		   
+
+		   $scope.slides = [];
+		   for (i = 0, len = data.files.length; i < len; i++){ 
+		       var file = data.files[i];
+			   if (file.type == "img") {
+				   var tpath = $scope.path.slice();
+				   tpath.push(file.name);
+				   var slide = {};
+				   var imgPath = tpath.join("/");
+				   if (imgPath.charAt(0) != "/") { imgPath = "/" + imgPath;}
+				   slide.name = file.name;
+				   slide.image = tpath.join("/");
+				   $scope.slides.push(slide)
+			   }
+		   }
+		   
+		   
 		});
-	});
-	
-	polkhistApp.controller('MembershipController', function($scope, $http) {
-		$http.get('/data/membership.json').then(function(res) {
-			$scope.rates = res.data;
-		});
-	});
-	
+		
+		$scope.showImage = function(slide){
+			$scope.currentSlide = slide;
+		}
+});
